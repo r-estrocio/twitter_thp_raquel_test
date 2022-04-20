f3b41b083be38cc4a3d5b1f04d361706acbbb771
Raquel Estrocio <r.estrocio@gmail.com>
2022-04-20 16:40:16 +0200

    Changes to files

--- lib/app.rb
+++ lib/app.rb
@@ -8,12 +8,17 @@ require 'dotenv'
 Dotenv.load('.env')
 
 # quelques lignes qui appellent les clés d'API de ton fichier .env
+def login_twitter()
 client = Twitter::REST::Client.new do |config|
  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ return client
 end
 
 # ligne qui permet de tweeter sur ton compte
-client.update('Mon premier tweet en Ruby !!!!')
+def my_first_tweet()
+  client = login_twitter
+  return client.update('My first tweet in Ruby !!!!')
+end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/app.rb'
+
 describe "the login_twitter method" do
   it "should return client, and client is not nil" do
     expect(login_twitter).not_to be_nil
